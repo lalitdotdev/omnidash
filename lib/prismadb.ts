1909f412c05ae4aae3e030ed532955b8a8176f0a
Lalit sharma <geekymonkcp742k@gmail.com>
2023-08-26 19:25:36 +0530

    Database setup

---
+++ lib/prismadb.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismadb = globalThis.prisma || new PrismaClient();
+
+// if statement whether use global this or intialize new prisma client instance
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prismadb;
+}
+
+export default prismadb;
