e26c43474ec4969f61d5aaa82eacb8de589e4923
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-02 22:40:52 +0530

    Define BillboardColumn type and columns for rendering billboard data table.

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type BillboardColumn = {
+  id: string;
+  label: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+];
