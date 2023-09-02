1153807f95afa1edf48a15680a1c14e8dab74c4e
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-02 22:23:51 +0530

    Create BillboardClient component for displaying and managing billboards with a data table and button for creating new billboard of store

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/Client.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Billboards are the main way to advertise your products and services. You can create a billboard by clicking on the button."
+        />
+
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4">Add New</Plus>
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} />
+    </>
+  );
+};
+
+export default BillboardClient;
