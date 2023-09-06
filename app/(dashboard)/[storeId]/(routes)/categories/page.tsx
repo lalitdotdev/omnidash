818eefeca61a22301d202edc820334f0df90f80d
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-06 20:07:35 +0530

    Implemented CategoriesPage component to fetch and display store categories with associated billboard data and passed to the CategoryClient component for rendering.

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,43 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import BillboardClient from "./components/Client";
+import { CategoryColumn } from "./components/columns";
+import CategoryClient from "./components/Client";
+
+const CategoriesPage = async ({
+  params,
+}: {
+  params: {
+    storeId: string;
+  };
+}) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map(item => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(new Date(item.createdAt), "MMMM do ,yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesPage;
