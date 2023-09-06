21a33b0a1c9b3099735ea18294764529d1240f92
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-06 20:21:35 +0530

    Implemented CellAction component for category-related actions such as copying the category ID, updating, and deleting categories.

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type CategoryColumn = {
+  id: string;
+  name: string;
+  billboardLabel: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({ row }) => row.original.billboardLabel,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
