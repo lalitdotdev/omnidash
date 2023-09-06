3faf28ae504aebf0def20294d27a9eff4c6d9bde
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-06 20:11:16 +0530

    Implemented CategoryPage component to fetch and display a single category with associated billboards and passed to form for updation and deletion.

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,31 @@
+import prismadb from "@/lib/prismadb";
+import { CategoryForm } from "./components/category-form";
+
+const CategoryPage = async ({
+  params,
+}: {
+  params: { categoryId: string; storeId: string };
+}) => {
+  const category = await prismadb.category.findFirst({
+    where: {
+      id: params.categoryId,
+    },
+  });
+
+  // fetch all the billboards for the store
+
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  return (
+    <div className="flex-col ">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm billboards={billboards} initialData={category} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
