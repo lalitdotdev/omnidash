d2e36bc89986c8fe37a52d52abd933e4ee04b324
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-06 20:00:23 +0530

    Implemented BillboardClient component for displaying billboards and API information.

--- app/(dashboard)/[storeId]/(routes)/billboards/components/Client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/Client.tsx
@@ -8,6 +8,7 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -32,6 +33,12 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading
+        title="API"
+        description="API calls for Billboards. You can also use the endpoints to get the billboards of a specific store."
+      />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
