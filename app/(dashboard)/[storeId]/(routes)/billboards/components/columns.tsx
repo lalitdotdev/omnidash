8572463a948ca610bebab7cc187a6012d41ff3b1
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-06 20:01:39 +0530

    Added BillboardColumn type and columns configuration for the DataTable component.

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -18,5 +19,9 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   {
     accessorKey: "createdAt",
     header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
   },
 ];
