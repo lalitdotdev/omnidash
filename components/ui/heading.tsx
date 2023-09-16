c94bafd17fb1ddc07c31b13bd49648f1a6983494
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-16 16:32:10 +0530

    UI components like command dialog form heading

---
+++ components/ui/heading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+}
+const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold trackin-tight">{title}</h2>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </div>
+  );
+};
+
+export default Heading;
