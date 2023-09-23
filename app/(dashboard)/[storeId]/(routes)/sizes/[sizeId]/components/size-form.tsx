cee66bf819c157791329fae4a4844e45824ac36b
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-23 18:55:18 +0530

    Create SizeForm component for managing size data with validation and API interactions

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import { AlertModal } from "@/components/modals/alert-modal";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Size } from "@prisma/client";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+
+interface SizeFormProps {
+  initialData: Size | null;
+}
+
+// formSchema -> SizeFormValues -> SizeForm using react hook form -> onSubmit -> update store
+const formSchema = z.object({
+  name: z.string().min(3).max(25).nonempty(),
+  value: z.string(),
+});
+
+type SizeFormValues = z.infer<typeof formSchema>;
+
+export const SizeForm: React.FC<SizeFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit Size" : "Create Size";
+
+  const description = initialData ? "Edit a size" : "Add a new sizes";
+
+  const toastMessage = initialData
+    ? "size updated successfully"
+    : "size created successfully";
+
+  const action = initialData ? "Save Changes" : "Create sizes";
+
+  const origin = useOrigin();
+  const form = useForm<SizeFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  // onDelete -> delete store -> refresh page -> redirect to root page (root layout will check if user has store and open createStore Modal if not found -> create store page will check if user has store and redirect to dashboard if found )
+
+  const onSubmit = async (data: SizeFormValues) => {
+    try {
+      setLoading(true);
+
+      //& if initialData is true then we are updating the store else we are creating a new store (initialData is null)
+
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/sizes/${params.sizeId}`,
+          data,
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/sizes`, data);
+      }
+
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      // Delete store
+      await axios.delete(`/api/${params.storeId}/sizes/${params.sizeId}`);
+      router.refresh();
+
+      router.push("/${params.storeId}/sizes");
+      toast.success("sizes deleted successfully");
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all products using this sizes first. ",
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+
+      {/* Form  and spreading the form using react hook form */}
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size Value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
