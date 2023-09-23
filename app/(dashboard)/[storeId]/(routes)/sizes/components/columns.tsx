4d480866bbcde31327842de97ea572d766fa915c
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-23 18:49:56 +0530

    Define SizeColumn type and columns for displaying Size data

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type SizeColumn = {
+  id: string;
+  name: string;
+  value: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<SizeColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+
+  {
+    accessorKey: "value",
+    header: "Value",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
