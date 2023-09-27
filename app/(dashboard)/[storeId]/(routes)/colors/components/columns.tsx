b8595dec3eeb1e5e015c58ed7a8deccc6a343e41
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 20:57:29 +0530

    define ColorColumn type and columns for displaying Color data

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+import { SizeColumn } from "../../sizes/components/columns";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type ColorColumn = {
+  id: string;
+  name: string;
+  value: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<SizeColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+
+  {
+    accessorKey: "value",
+    header: "Value",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.value}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{
+            backgroundColor: row.original.value,
+          }}
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
