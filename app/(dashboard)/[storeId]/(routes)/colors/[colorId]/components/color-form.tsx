d213c204a8b14eaf78ccfcad3c6955ba46334545
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 20:55:49 +0530

    Create ColorForm component for managing color data with validation and API interactions

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import { AlertModal } from "@/components/modals/alert-modal";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Color, Size } from "@prisma/client";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+
+interface ColorFormProps {
+  initialData: Color | null;
+}
+
+// formSchema -> ColorFormValues -> ColorForm using react hook form -> onSubmit -> update store
+const formSchema = z.object({
+  name: z.string().min(3).max(25).nonempty(),
+  value: z
+    .string()
+    .min(4)
+    .regex(/^#([0-9a-f]{3}){1,2}$/i, {
+      message: "String must be a valid hex color",
+    }),
+});
+
+type ColorFormValues = z.infer<typeof formSchema>;
+
+export const ColorForm: React.FC<ColorFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit Color" : "Create Color";
+
+  const description = initialData ? "Edit color" : "Add a new color";
+
+  const toastMessage = initialData
+    ? "color updated successfully"
+    : "color created successfully";
+
+  const action = initialData ? "Save Changes" : "Create colors";
+
+  const form = useForm<ColorFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  // onDelete -> delete store -> refresh page -> redirect to root page (root layout will check if user has store and open createStore Modal if not found -> create store page will check if user has store and redirect to dashboard if found )
+
+  const onSubmit = async (data: ColorFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/colors/${params.colorId}`,
+          data,
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/colors`, data);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/colors`);
+      toast.success(toastMessage);
+    } catch (error: any) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/colors/${params.colorId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/colors`);
+      toast.success("Color deleted.");
+    } catch (error: any) {
+      toast.error("Make sure you removed all products using this color first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+
+      {/* Form  and spreading the form using react hook form */}
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Color Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-x-4 ">
+                      <Input
+                        disabled={loading}
+                        placeholder="Color Value"
+                        {...field}
+                      />
+                      <div
+                        className="h-6 w-6 rounded-full border"
+                        style={{
+                          backgroundColor: field.value,
+                        }}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
