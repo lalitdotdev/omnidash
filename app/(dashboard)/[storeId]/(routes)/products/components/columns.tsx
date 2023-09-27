d94474b23eb814b36a1e9b01f9b828b21a915ceb
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 21:40:15 +0530

    Define product table columns and actions in ProductColumn and columns files for improved UI functionality

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { CellAction } from "./cell-action";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  price: string;
+  category: string;
+  size: string;
+  color: string;
+  createdAt: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
