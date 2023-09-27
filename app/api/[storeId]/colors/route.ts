4ace8321e689f1b6d1d478f8149a6893433dcc93
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 21:08:26 +0530

    Implemented POST and GET API routes for Color management with authentication, validation, and error handling

---
+++ app/api/[storeId]/colors/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const color = await prismadb.color.create({
+      data: {
+        name,
+        value,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLORS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const colors = await prismadb.color.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(colors);
+  } catch (error) {
+    console.log("[COLORS_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
