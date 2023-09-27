57f7a454618d75faf721dc4adf340bf842970097
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 21:38:35 +0530

    Create ProductsPage component to display a list of products

---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,45 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+
+import { ProductColumn } from "./components/columns";
+import ProductsClient from "./components/Client";
+
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedProducts: ProductColumn[] = products.map(item => ({
+    id: item.id,
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price.toNumber()),
+    category: item.category.name,
+    size: item.size.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductsClient data={formattedProducts} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
