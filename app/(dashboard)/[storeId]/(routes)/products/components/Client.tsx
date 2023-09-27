10fafd5e2985125c399195e830743bdb450ad158
Lalit sharma <geekymonkcp742k@gmail.com>
2023-09-27 21:41:16 +0530

    Create a ProductsClient component with UI elements for managing products and API calls

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/Client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ProductColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+const ProductsClient: React.FC<ProductClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Products (${data.length})`}
+          description="Manage products for your store."
+        />
+
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4">Add New</Plus>
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Products." />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId" />
+    </>
+  );
+};
+
+export default ProductsClient;
