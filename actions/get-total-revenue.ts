e386829c38375e640b6ba5db71fa1a9a13aa8606
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 10:31:27 +0530

    Create getTotalRevenue action function to calculate store's total revenue

---
+++ actions/get-total-revenue.ts
@@ -0,0 +1,26 @@
+import prismadb from "@/lib/prismadb";
+
+export const getTotalRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  const totalRevenue = paidOrders.reduce((total, order) => {
+    const orderTotal = order.orderItems.reduce((orderSum, item) => {
+      return orderSum + item.product.price.toNumber();
+    }, 0);
+    return total + orderTotal;
+  }, 0);
+
+  return totalRevenue;
+};
