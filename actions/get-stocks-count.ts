14e96d2e4f1f99f38d840f70e736f722d2771eed
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 10:49:10 +0530

    Create getStockCount function to retrieve store's stock count enhances the application's ability to provide information about the current stock availability for a specific store, aiding in inventory management and user experience

---
+++ actions/get-stocks-count.ts
@@ -0,0 +1,12 @@
+import prismadb from "@/lib/prismadb";
+
+export const getStockCount = async (storeId: string) => {
+  const stockCount = await prismadb.product.count({
+    where: {
+      storeId,
+      isArchived: false,
+    },
+  });
+
+  return stockCount;
+};
