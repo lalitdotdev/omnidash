480aa5956fcf40a8cd4a3562a710ad0fbb4b4936
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 09:54:25 +0530

    Currency Formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const rupeeFormatter = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+});
+
+// Create a currency formatter for US dollars (USD)
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency", // Format the number as currency
+  currency: "USD", // Use US dollars as the currency symbol
+});
