e0244e4f908f1b3d28d8bc543a9836820a8dc6dd
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 10:15:00 +0530

    fetches and formats orders from the database based on the provided storeId and displays the formatted order information, including order ID, phone number, address, ordered products, total price, payment status, and creation date

---
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,51 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+
+import { OrderColumn } from "./components/columns";
+import OrderClient from "./components/Client";
+
+const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedOrders: OrderColumn[] = orders.map(item => ({
+    id: item.id,
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems
+      .map(orderItem => orderItem.product.name)
+      .join(", "),
+    totalPrice: formatter.format(
+      item.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0),
+    ),
+    isPaid: item.isPaid,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrderClient data={formattedOrders} />
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
