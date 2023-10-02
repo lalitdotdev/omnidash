c38a9b0372600f619dc6f7dbe1d03de35d81e458
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 10:28:45 +0530

    Define OrderColumn type and columns configuration for orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  products: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Products",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total Price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+];
