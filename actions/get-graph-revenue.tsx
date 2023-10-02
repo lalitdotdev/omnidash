e8612427623c875df59200491e311b5c2155802d
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-02 10:51:03 +0530

    Create getGraphRevenue function for fetching monthly revenue data queries the database to fetch paid orders, including order items and product details resulting data is formatted as an array of objects with month names and total revenue, suitable for visualization in a graph.

---
+++ actions/get-graph-revenue.tsx
@@ -0,0 +1,62 @@
+import prismadb from "@/lib/prismadb";
+
+interface GraphData {
+  name: string;
+  total: number;
+}
+
+export const getGraphRevenue = async (
+  storeId: string,
+): Promise<GraphData[]> => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  const monthlyRevenue: { [key: number]: number } = {};
+
+  // Grouping the orders by month and summing the revenue
+  for (const order of paidOrders) {
+    const month = order.createdAt.getMonth(); // 0 for Jan, 1 for Feb, ...
+    let revenueForOrder = 0;
+
+    for (const item of order.orderItems) {
+      revenueForOrder += item.product.price.toNumber();
+    }
+
+    // Adding the revenue for this order to the respective month
+    monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
+  }
+
+  // Converting the grouped data into the format expected by the graph
+  const graphData: GraphData[] = [
+    { name: "Jan", total: 0 },
+    { name: "Feb", total: 0 },
+    { name: "Mar", total: 0 },
+    { name: "Apr", total: 0 },
+    { name: "May", total: 0 },
+    { name: "Jun", total: 0 },
+    { name: "Jul", total: 0 },
+    { name: "Aug", total: 0 },
+    { name: "Sep", total: 0 },
+    { name: "Oct", total: 0 },
+    { name: "Nov", total: 0 },
+    { name: "Dec", total: 0 },
+  ];
+
+  // Filling in the revenue data
+  for (const month in monthlyRevenue) {
+    graphData[parseInt(month)].total = monthlyRevenue[parseInt(month)];
+  }
+
+  return graphData;
+};
