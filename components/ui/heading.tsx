b7044dde42df1a56dc9bdeccd554922bf221256b
Lalit sharma <geekymonkcp742k@gmail.com>
2024-01-09 14:49:46 +0530

    feat(dynamic Headings and avatar UI Component): Developed a 'Heading' component for rendering titles and descriptions with customizable styles, and an 'Avatar' component incorporating 'AvatarImage' and 'AvatarFallback' parts, leveraging '@radix-ui/react-avatar' for flexible avatar management.

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface HeadingProps {
   title: string;
@@ -7,7 +7,7 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
-      <h2 className="text-3xl font-bold trackin-tight">{title}</h2>
+      <h2 className="text-2xl font-semibold trackin-tight">{title}</h2>
       <p className="text-sm text-muted-foreground">{description}</p>
     </div>
   );
